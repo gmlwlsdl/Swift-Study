ed51a64a4fa2900edc57392bdbd6bdbae76d0ced
gmlwlsdl <heejin094@naver.com>
2024-09-30 21:28:11 +0900

    feat: 테이블셀 클릭 기능 추가

--- HelloWorld/TableController.swift
+++ HelloWorld/TableController.swift
@@ -18,14 +18,25 @@ class TableController: UIViewController, UITableViewDataSource, UITableViewDeleg
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 무엇? 반복하지 않을까 (위의 수만큼)
         // 셀 만들기는 2가지 방법이 있음
-        // 방법 1. 임의의 셀 만들기
         
-        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        // 방법 1. 임의의 셀 만들기 > 연습
         
-        cell.textLabel?.text = "\(indexPath.row)"
+        // let cell = UITableViewCell.init(style: .default, reuseIdentifier: "TableCellType1")
+        // cell.textLabel?.text = "\(indexPath.row)"
+        
+        // 방법 2. 스토리보드 + id > 실전
+        // as? as! - 부모 자식 친자확인
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Type1", for: indexPath) as! Type1
+        
+        cell.label?.text = "\(indexPath.row)"
         
         return cell
         
+    }
+    
+    // 클릭
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row) 번째 셀 클릭")
     }
     
     // 정갈하게 데이터를 보여주기 위함 ex) 전화번호부
---
+++ HelloWorld/Type1.swift
@@ -0,0 +1,13 @@
+//
+//  Type1.swift
+//  HelloWorld
+//
+//  Created by 희진 on 9/30/24.
+//
+
+import UIKit
+
+class Type1: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+}
