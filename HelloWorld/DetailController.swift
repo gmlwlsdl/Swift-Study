be3aecbec2885093a83c337ece48d7f8dee5bf53
gmlwlsdl <heejin094@naver.com>
2024-09-30 20:08:18 +0900

    feat: webview 기능 추가

--- HelloWorld/DetailController.swift
+++ HelloWorld/DetailController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import WebKit
 
 class DetailController: UIViewController {
     
+    @IBOutlet weak var webview: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // url 주소 찾기 > request로 > 보이기
+        let urlString = "https://www.google.com"
+        if let url = URL(string: urlString) { // unwrap
+            let urlReq = URLRequest(url: url)
+            webview.load(urlReq)
+        }
     }
+    
 }
